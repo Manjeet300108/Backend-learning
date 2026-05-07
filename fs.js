4b0dc72a63013e0274751394b1db3ce4be3d660d
Manjeet300108 <singh300108@gmail.com>
2026-05-07 18:09:52 +0530

    basic of js and fs module

---
+++ fs.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+
+//?writeFile
+// fs.writeFile("hello.txt", 'Hello Myself Manjeet', function (err) {
+//     if (err) console.log(err)
+//     else console.log('done')
+// })
+
+//?appendFile 
+// fs.appendFile('hello.txt', 'i am learning backend', function (err) {
+//     if (err) console.log(err)
+//     else console.log("append done")
+// })
+
+//?rename
+// fs.rename('hello.txt', 'new.txt', function (err) {
+//     if (err) console.log(err)
+//     else console.log('rename done')
+// })
+
+// fs.appendFile('new.txt',' Hello My self Manjeet singh',function(err){
+//     if(err) console.log(err)
+//         else console.log('done')
+// })
+
+//?copy file
+// fs.copyFile('new.txt', './copy/copy.txt', function (err) {
+//     if (err) console.log(err)
+//     else console.log('done')
+// })
+
+//?unlink file --> delete file
+// fs.unlink('new.txt',function(err){
+//     if(err) console.log(err)
+//         else console.log('done')
+// })
+
+//?Remove folder
+// fs.rm('./copy',{recursive:true,force : true},function(err){
+//     if(err) console.log(err.message)
+//         else console.log('removed')
+// })
+
+//?Read file
+// fs.readFile('hello.txt', "utf8", function (err, data) {
+//     if (err) throw err;
+//     else console.log(data)
+// })
+
+//?Make folder
+// fs.mkdir('copy', function (err) {
+//     if (err) throw err
+//     else console.log("folder created")
+// })
+
+// fs.copyFile('hello.txt','./copy/new.txt',function(err){
+//     if(err) throw err;
+//     else console.log('file copied');
+// })
+
+//?Read file in a folder
+// fs.readdir('./copy',function(err,files){
+//     if(err) throw err;
+//     else console.log(files)
+// })
+
+//?Read file from a folder
+// fs.readdir('./copy',function(err,files){
+//     files.forEach(function(file){
+//         fs.readFile(`./copy/${file}`,'utf-8',function(err,data){
+//             if(err) throw err;
+//             else console.log(`data of ${file}`)
+//             console.log(data)
+//         })
+//     })
+// })
---
+++ script.js
@@ -0,0 +1,73 @@
+// var arr = [0,1,2,3,4,5,6,7,8,9];
+
+//  arr.forEach(function(val){
+//    console.log(val + " Hello");
+
+// })
+
+// let newArr = arr.map(function (val) {
+//     return val + 3
+// })
+
+// console.log(newArr);
+
+// let newArr = arr.filter(function(val){
+    //     if(val%2 !== 0) return true;
+    // })
+    
+    // console.log(newArr);
+    
+    // let value = arr.find(function(val){
+        //     if(val ===12 ) return val;
+        // })
+        
+        // console.log(value);
+
+
+// let Manu = 12;        
+// let obj = {
+//     name : Manu
+// }
+
+// Object.freeze(obj)
+// obj.name = "Manjeet"
+
+
+
+// console.log(obj.name);
+
+
+// function abcd(){
+//     let a = 12;
+//     function defg(){
+//         console.log(a);
+//     }
+//     defg()
+// }
+// abcd()
+
+
+// let a = 12;
+
+// function abcd(){
+//     console.log(a)
+// }
+
+// function defg(){
+//     let a = 13;
+//     abcd();
+// }
+
+// defg();
+
+
+// function abcd(){
+//     let a = 12;
+//     return function(){
+//         console.log(a);
+//     }
+// }
+
+// let ans = abcd();
+// ans()
+
